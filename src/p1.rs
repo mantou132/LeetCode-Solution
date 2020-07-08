78c202570e4a23c687f6d25767ba6320d4b4e410
mantou132 <709922234@qq.com>
2020-07-08 15:26:18 +0800

    improve p1

--- src/p1.rs
+++ src/p1.rs
@@ -1,31 +1,31 @@
 struct Solution {}
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        use std::collections::HashMap;
-        let length = nums.len() as i32;
-        let mut map = HashMap::new();
-        for i in 0..length {
-            map.insert(nums[i as usize], i);
+  pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    use std::collections::HashMap;
+    let length = nums.len() as i32;
+    let mut map = HashMap::new();
+    for i in 0..length {
+      let complement = target - nums[i as usize];
+      match map.get(&complement) {
+        Some(&n) if n != i => {
+          return vec![i, n];
         }
-        for i in 0..length {
-            let complement = target - nums[i as usize];
-            match map.get(&complement) {
-                Some(&n) if n != i => {
-                    return vec![i, n];
-                },
-                _ => (),
-            }
-        }
-        vec![0, 0]
+        _ => (),
+      }
+      map.insert(nums[i as usize], i);
     }
+    vec![0, 0]
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Solution;
-    #[test]
-    fn it_works() {
-        assert_eq!(Solution::two_sum(vec![5, 4, 2, 3, 6], 10), vec![1,4]);
-    }
+  #[test]
+  fn it_works() {
+    assert_eq!(
+      super::Solution::two_sum(vec![5, 4, 2, 3, 6], 10),
+      vec![4, 1]
+    );
+  }
 }
